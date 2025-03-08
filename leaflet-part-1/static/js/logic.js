20cb8da829191e804d02a78b79643e34d58f3b37
Ilir <ilir.hajdari111@gmail.com>
2025-03-08 00:53:51 -0500

    added base maps to logic.js, as well as map object

---
+++ leaflet-part-1/static/js/logic.js
@@ -0,0 +1,60 @@
+// create the tile layers for the backgrounds of the map
+const defaultMap = L.tileLayer(
+  "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+  {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }
+);
+
+// grayscale layer for the map
+const grayscale = L.tileLayer(
+  "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}",
+  {
+    minZoom: 0,
+    maxZoom: 20,
+    attribution:
+      '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    ext: "png",
+  }
+);
+
+const WorldImagery = L.tileLayer(
+  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  {
+    attribution:
+      "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+  }
+);
+
+const NatGeoWorldMap = L.tileLayer(
+  "https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}",
+  {
+    attribution:
+      "Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC",
+    maxZoom: 16,
+  }
+);
+
+// make a baseMaps object
+let basemaps = {
+  Default: defaultMap,
+  Grayscale: grayscale,
+  "World Imagery": WorldImagery,
+  "Nat Geo Map": NatGeoWorldMap,
+};
+
+// make a map object
+let myMap = L.map("map", {
+  center: [36.7783, -119.4179],
+  zoom: 3,
+  layers: [defaultMap, grayscale, WorldImagery, NatGeoWorldMap],
+});
+
+// add the default map to the map object
+defaultMap.addTo(myMap);
+//grayscale.addTo(myMap);
+
+// add the layer control
+L.control.layers(basemaps).addTo(myMap);
